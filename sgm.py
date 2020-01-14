8aa86eba91006bcb66c7490c09a325956923b67b
Chetan Srinivasa Kumar <chetanskumar1991@gmail.com>
2020-01-14 22:23:34 +0100

    Added Local Matching, and all three Cost Volume computations.

--- sgm.py
+++ sgm.py
@@ -25,9 +25,23 @@ def compute_cost_volume_sad(left_image, right_image, D, radius):
     :param radius: Radius of the filter
     :return: cost volume of size (H,W,D)
     """
-    # TODO
-    return
-
+    H = left_image.shape[0]
+    W = right_image.shape[1]
+    cv_sad = np.zeros((H, W, D))
+    
+    for d in range(0, D):
+        #Translate image by d, and compute similarity for that slice of cost volume. 
+        tr_right_image = np.roll(right_image, d, axis = 0)
+        diff_im = np.abs(left_image - tr_right_image) ##Absolute difference
+        padded_diff_im = np.zeros((diff_im.shape[0] + 2 * radius , diff_im.shape[1] + 2 * radius))
+        padded_diff_im[radius : radius + diff_im.shape[0], radius : radius + diff_im.shape[1]] = diff_im
+        padded_diff_im_windows = view_as_windows(padded_diff_im, (radius * 2, radius * 2))
+        
+        padded_diff_im_windows = padded_diff_im_windows.reshape((padded_diff_im_windows.shape[0], padded_diff_im_windows.shape[1], -1))
+        cv_sad[:, :, d] = np.sum(padded_diff_im_windows, axis = 2)[0 : H, 0: W]
+        print("Computed SAD for disparity slice, ", d)
+                
+    return cv_sad
 
 def compute_cost_volume_ssd(left_image, right_image, D, radius):
     """
@@ -38,8 +52,23 @@ def compute_cost_volume_ssd(left_image, right_image, D, radius):
     :param radius: Radius of the filter
     :return: cost volume of size (H,W,D)
     """
-    # TODO
-    return
+    H = left_image.shape[0]
+    W = right_image.shape[1]
+    cv_ssd = np.zeros((H, W, D))
+    
+    for d in range(0, D):
+        #Translate image by d, and compute similarity for that slice of cost volume. 
+        tr_right_image = np.roll(right_image, d, axis = 0)
+        diff_im = (left_image - tr_right_image) ** 2 #Squared difference. 
+        padded_diff_im = np.zeros((diff_im.shape[0] + 2 * radius , diff_im.shape[1] + 2 * radius))
+        padded_diff_im[radius : radius + diff_im.shape[0], radius : radius + diff_im.shape[1]] = diff_im
+        padded_diff_im_windows = view_as_windows(padded_diff_im, (radius * 2, radius * 2))
+        
+        padded_diff_im_windows = padded_diff_im_windows.reshape((padded_diff_im_windows.shape[0], padded_diff_im_windows.shape[1], -1))
+        cv_ssd[:, :, d] = np.sum(padded_diff_im_windows, axis = 2)[0 : H, 0: W]
+        print("Computed SSD for disparity slice, ", d)
+                
+    return cv_ssd
 
 
 def compute_cost_volume_ncc(left_image, right_image, D, radius):
@@ -51,8 +80,40 @@ def compute_cost_volume_ncc(left_image, right_image, D, radius):
     :param radius: Radius of the filter
     :return: cost volume of size (H,W,D)
     """
-    # TODO
-    return
+    H = left_image.shape[0]
+    W = right_image.shape[1]
+    cv_ncc = np.zeros((H, W, D))
+
+    padded_left_image = np.zeros((left_image.shape[0] + 2 * radius , left_image.shape[1] + 2 * radius))
+    padded_left_image[radius : radius + left_image.shape[0], radius : radius + left_image.shape[1]] = left_image
+    padded_left_image_windows = view_as_windows(padded_left_image, (radius * 2, radius * 2))
+    padded_left_image_windows = padded_left_image_windows.reshape((padded_left_image_windows.shape[0], padded_left_image_windows.shape[1], -1))
+    pv = padded_left_image_windows
+    p_mean = np.mean(pv, axis = 2)
+    p_mean_3d = np.repeat(p_mean[:, :, np.newaxis], pv.shape[2], axis = 2)
+    p = pv - p_mean_3d
+    
+    for d in range(0, D):
+        #Translate image by d, and compute similarity for that slice of cost volume. 
+        tr_right_image = np.roll(right_image, d, axis = 0)
+        padded_tr_right_image = np.zeros((tr_right_image.shape[0] + 2 * radius , tr_right_image.shape[1] + 2 * radius))
+        padded_tr_right_image[radius : radius + tr_right_image.shape[0], radius : radius + tr_right_image.shape[1]] = tr_right_image
+        padded_tr_right_image_windows = view_as_windows(padded_tr_right_image, (radius * 2, radius * 2))
+        padded_tr_right_image_windows = padded_tr_right_image_windows.reshape((padded_tr_right_image_windows.shape[0], padded_tr_right_image_windows.shape[1], -1))
+        qv = padded_tr_right_image_windows
+        
+        q_mean = np.mean(qv, axis = 2)
+        q_mean_3d = np.repeat(q_mean[:, :, np.newaxis], qv.shape[2], axis = 2)
+        q = qv - q_mean_3d
+        
+        pq = p * q
+        p2q2 = (p ** 2) * (q ** 2)
+        
+        cv_ncc[:, :, d] = (np.sum(pq, axis = 2) / np.sqrt(np.sum(p2q2, axis = 2)))[0: H, 0: W]
+        
+        print("Computed NCC for disparity slice, ", d)
+                
+    return cv_ncc
 
 
 def get_pairwise_costs(H, W, D, weights=None):
@@ -82,8 +143,8 @@ def compute_sgm(cv, f):
 
 def main():
     # Load input images
-    im0 = imread("data/Adirondack_left.png")
-    im1 = imread("data/Adirondack_right.png")
+    im0 = imread("Images/Adirondack_left.png")
+    im1 = imread("Images/Adirondack_right.png")
 
     im0g = rgb2gray(im0)
     im1g = rgb2gray(im1)
@@ -95,18 +156,24 @@ def main():
     plt.tight_layout()
 
     # Use either SAD, NCC or SSD to compute the cost volume
+#    cv = compute_cost_volume_sad(im0g, im1g, 64, 5)
+#    cv = compute_cost_volume_ssd(im0g, im1g, 64, 5)
     cv = compute_cost_volume_ncc(im0g, im1g, 64, 5)
 
+    
+    #Compute winner takes all. 
+    disp_wta = np.argmin(cv, axis = 2)
+    
     # Compute pairwise costs
-    H, W, D = cv.shape
-    f = get_pairwise_costs(H, W, D)
+#    H, W, D = cv.shape
+#    f = get_pairwise_costs(H, W, D)
 
     # Compute SGM
-    disp = compute_sgm(cv, f)
+#    disp = compute_sgm(cv, f)
 
     # Plot result
     plt.figure()
-    plt.imshow(disp)
+    plt.imshow(disp_wta)
     plt.show()
 
 
